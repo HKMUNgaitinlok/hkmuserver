01732e91c60cd8177ccb8c7dd502efe7b5b6f86d
HKMUNgaitinlok <118424251+HKMUNgaitinlok@users.noreply.github.com>
2022-11-17 15:22:52 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 'use strict';
 var http = require('http');
 var port = process.env.PORT || 1337;
-//set-executionpolicy remotesigned
-
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.end('Hello 1111111111World1\n');
 }).listen(port);
