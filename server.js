e4136de78639a03335f63f39c3bf14c655fe63a1
HKMUNgaitinlok <118424251+HKMUNgaitinlok@users.noreply.github.com>
2022-11-18 11:24:09 +0800

    Add files via upload
    4

--- server.js
+++ server.js
@@ -1,8 +1,217 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+//HTML 
+const html = require('html');
+const url = require('url');
+const assert = require('assert');
+//File
+const fs = require('fs');
+const formidable = require('express-formidable');
+//MongoDB
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello 11111111122221World1\n');
-}).listen(port);
+const mongourl = 'mongodb+srv://12345:12345@cluster0.cvrczfj.mongodb.net/?retryWrites=true&w=majority';
+const dbName = 'test';
+
+const express = require('express');
+const app = express();
+const session = require('cookie-session');
+
+const bodyParser = require('body-parser');
+
+const { Buffer } = require('safe-buffer');
+
+var users = new Array(
+    { name: "admin", password: "admin" },
+    { name: "student", password: "student" }
+);
+
+var DOC = {};
+
+//Main Body
+app.set('view engine', 'ejs');
+app.use(formidable());
+//Middleware
+app.use(bodyParser.json());
+//Cookie
+app.use(session({
+    userid: "session",
+    keys: ['th1s!sA5ecretK3y'],
+    //maxAge: 90 * 24 * 60 * 60 * 1000
+}));
+
+//functions
+//create new inventory docs
+const createDocument = (db, createDoc, callback) => {
+    const client = new MongoClient(mongourl);
+    client.connect((err) => {
+        assert.equal(null, err);
+        console.log("Connected successfully to the DB server.");
+        const db = client.db(dbName);
+
+        db.collection('inventory').insertOne(createDoc, (error, results) => {
+            if (error) throw error;
+            console.log(results);
+            //callback();
+        });
+    });
+}
+//find
+const findDocument = (db, criteria, callback) => {
+    let cursor = db.collection('inventory').find(criteria);
+    console.log(`findDocument: ${JSON.stringify(criteria)}`);
+    cursor.toArray((err, docs) => {
+        assert.equal(err, null);
+        console.log(`findDocument: ${docs.length}`);
+        callback(docs);
+    });
+}
+const handle_Find = (req, res, criteria) => {
+    const client = new MongoClient(mongourl);
+    client.connect((err) => {
+        assert.equal(null, err);
+        console.log("Connected successfully to the DB server.");
+        const db = client.db(dbName);
+        //callback()
+        findDocument(db, {}, (docs) => {
+            client.close();
+            console.log("Closed DB connection.");
+            res.status(200).render('home', { name: `${req.session.userid}`, ninventory: docs.length, inventory: docs });
+        });
+    });
+}
+
+//handling requests
+app.get('/', (req, res) => {
+    if (!req.session.authenticated) {
+        console.log("...Not authenticated; directing to login");
+        res.redirect("/login");
+    }
+    console.log("...Hello, welcome back");
+    handle_Find(req, res, {});
+});
+
+//login
+app.get('/login', (req, res) => {
+    console.log("...Welcome to login page");
+    res.sendFile(__dirname + '/public/login.html');
+    res.status(200).render("login");
+});
+app.post('/login', (req, res) => {
+    console.log("...Handling your login request");
+    users.forEach((user) => {
+        if (user.name == req.fields.username && user.password == req.fields.password) {
+            req.session.authenticated = true;
+            req.session.userid = req.fields.username;
+            console.log(req.session.userid);
+            res.status(200).redirect("/home");
+        }
+    });
+    res.redirect("/");
+});
+app.use((req, res, next) => {
+    console.log("...Checking login status");
+    if (req.session.authenticated) {
+        next();
+    } else {
+        res.redirect("/login");
+    }
+});
+app.get('/logout', (req, res) => {
+    req.session = null;
+    req.authenticated = false;
+    res.redirect('/');
+});
+
+//Home page
+app.get('/home', (req, res) => {
+    console.log("...Welcome to the home page!")
+    const client = new MongoClient(mongourl);
+    client.connect((err) => {
+        assert.equal(null, err);
+        console.log("Connected successfully to the DB server.");
+        const db = client.db(dbName);
+        //callback()
+        findDocument(db, {}, (docs) => {
+            client.close();
+            console.log("Closed DB connection.");
+            res.status(200).render('home', { name: `${req.session.userid}`, ninventory: docs.length, inventory: docs });
+        });
+    });
+    res.status(200).render('home', {name: `${req.session.userid}`});
+});
+
+//create
+app.get('/create', (req, res) => {
+    res.status(200).render("create");
+});
+app.post('/create', (req, res) => {
+    console.log("...create a new document!");
+    const client = new MongoClient(mongourl);
+    client.connect((err) => {
+        assert.equal(null, err);
+        console.log("Connected successfully to the DB server.");
+        const db = client.db(dbName);
+
+        // Get a timestamp in seconds
+        var timestamp = Math.floor(new Date().getTime() / 1000);
+        // Create a date with the timestamp
+        var timestampDate = new Date(timestamp * 1000);
+
+        // Create a new ObjectID with a specific timestamp
+        var objectId = new ObjectID(timestamp);
+
+        DOC["_id"] = objectId;
+
+        DOC['inv_id'] = "";
+        DOC['name'] = req.fields.name;
+        DOC['inv_type'] = req.fields.inv_type;
+        DOC['quantity'] = req.fields.quantity;
+        DOC['owner'] = `${req.session.userid}`;
+        console.log("...putting data into DOC");
+        var adoc = {};
+        adoc['building'] = req.fields.building;
+        adoc['country'] = req.fields.country;
+        if (req.fields.latitude && req.fields.longitude) {
+            adoc['coord'] = [req.fields.latitude, req.fields.longitude];
+        }
+        adoc['street'] = req.fields.street;
+        adoc['zipcode'] = req.fields.zipcode;
+        DOC['address'] = adoc;
+
+        var pdoc = {};
+        if (req.files.photo && req.files.photo.size > 0 && (pdoc['mimetype'] == 'image/jpeg' || pdoc['mimetype'] == 'image/png')) {
+            fs.readFile(req.files.photo.path, (err, data) => {
+                assert.equal(err, null);
+                pdoc['title'] = req.fields.title;
+                pdoc['data'] = new Buffer.from(data).toString('base64');
+                pdoc['mimetype'] = req.files.photo.type;
+
+            });
+        }
+        DOC['photo'] = pdoc;
+
+        if (DOC.name && DOC.owner) {
+            console.log("...Creating the document");
+            createDocument(db, DOC, (docs) => {
+                client.close();
+                console.log("Closed DB connection");
+                res.status(200).render('info', { message: "Document created successfully!" });
+            });
+        } else {
+            client.close();
+            console.log("Closed DB connection");
+            res.status(200).render('info', { message: "Invalid entry - Name & Owner is compulsory!" });
+        }
+    });
+    client.close();
+    console.log("Closed DB connection");
+    res.status(200).render('info', { message: "Document created" });
+});
+
+
+
+app.get('/*', (req, res) => {
+    res.status(404).render("info", { message: `${req.path} - Unknown request!` })
+});
+
+app.listen(process.env.PORT || 8099);
