080b1959a9e4d4671ddfa79a166b185f83857f66
HKMUNgaitinlok <118424251+HKMUNgaitinlok@users.noreply.github.com>
2022-11-18 12:10:18 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,58 +1,11 @@
-//HTML 
-const html = require('html');
-const url = require('url');
+var http = require("http");
 
-app.set('view engine', 'ejs');
-app.use(formidable());
-
-//Middleware
-app.use(bodyParser.json());
-//Cookie
-app.use(session({
-    userid: "session",  
-    keys: ['th1s!sA5ecretK3y'],
-    //maxAge: 90 * 24 * 60 * 60 * 1000
-}));
-
-//handling requests
-app.get('/', (req, res)=>{
-    if(!req.session.authenticated){
-        console.log("...Not authenticated; directing to login");
-        res.redirect("/login");
-    }
-    console.log("...Hello, welcome back");
-    handle_Find(req, res, {});
-});
-//login
-app.get('/login', (req, res)=>{
-    console.log("...Welcome to login page");
-    res.sendFile(__dirname + '/public/login.html');
-    res.status(200).render("login");
-});
-
-app.post('/login', (req, res)=>{
-    console.log("...Handling your login request");
-    users.forEach((user) => {
-		if (user.name == req.fields.username && user.password == req.fields.password) {
-        req.session.authenticated = true;
-        req.session.userid = req.fields.username;
-        console.log(req.session.userid);
-        res.status(200).redirect("/home");
-        }
-    });
-    res.redirect("/");
-});
-app.use((req, res, next) => {
-    console.log("...Checking login status");
-    if (req.session.authenticated){
-      next();
-    } else {
-      res.redirect("/login");
-    }
-});
-
-app.get('/logout', (req, res)=>{
-    req.session = null;
-    req.authenticated = false;
-    res.redirect('/');
-});
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
