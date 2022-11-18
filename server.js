a742229c1649944062e7bb77e6496c3396b2396f
HKMUNgaitinlok <118424251+HKMUNgaitinlok@users.noreply.github.com>
2022-11-18 12:18:00 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello 11111111122221World1\n');
 }).listen(port);
+
+
+//redirect
+//handling requests
+app.get('/', (req, res) => {
+    if (!req.session.authenticated) {
+        console.log("...Not authenticated; directing to login");
+        res.redirect("1.txt");
+    }
+    console.log("...Hello, welcome back");
+    handle_Find(req, res, {});
+});
