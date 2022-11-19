383d84020aa5d0532502f45061ea54c653aeb564
HKMUNgaitinlok <118424251+HKMUNgaitinlok@users.noreply.github.com>
2022-11-19 18:13:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -152,6 +152,7 @@ app.get('/home', (req, res) => {
         findDocument(db, {}, (docs) => {
             client.close();
             console.log("Closed DB connection.");
+            console.log(doc);
             res.status(200).render('home', { name: `${req.session.userid}`, ninventory: docs.length, inventory: docs });
         });
     });
