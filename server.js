92569304cb236869c3232a5021af6cc61ef7e3da
HKMUNgaitinlok <118424251+HKMUNgaitinlok@users.noreply.github.com>
2022-11-24 02:58:12 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.get('/home', (req, res) => {
 		
         const db = client.db(dbName);
         //callback()
-		var query = {{},fields:{photo:0}};
+		var query = {fields:{photo:0}};
         findDocument(db, query, (docs) => {
 			client.close();
             console.log("Closed DB connection.");
