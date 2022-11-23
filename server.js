e0e74f041454370fa71d68fec4b8c5ade4455ee9
HKMUNgaitinlok <118424251+HKMUNgaitinlok@users.noreply.github.com>
2022-11-23 15:01:37 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -218,25 +218,9 @@ app.get('/edit', (req, res) => {
 			client.close();
             console.log("Closed DB connection.");
             console.log(docs);
-			res.status(200).render("edit");
+			res.status(200).render("edit", {inventory: docs});
             //res.status(200).render('home',{name: `${req.session.userid}` ,ninventory: docs.length, inventory: docs});
         });
-		/*
-		const db = client.db(dbName);
-		let parsedURL = url.parse(req.url,true);
-		console.log(parsedURL.query.id);
-			console.log("111111111111");
-		let cursor = db.collection('inventory').find({"_id":parsedURL.query.id});
-		console.log("22222222222222222");
-		cursor.toArray((err,docs) => {
-			console.log("333333333333333333");
-			assert.equal(null,err);
-			console.log("Closed DB connection.");
-			console.log(docs);
-		});
-		
-		console.log("Connected successfully to the DB server.");
-		*/
 	});
 	
 });
